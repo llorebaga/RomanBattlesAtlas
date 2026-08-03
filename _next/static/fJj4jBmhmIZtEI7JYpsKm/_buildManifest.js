2884c70f8e1651ac525f29a07de6eb9d9a774909
Llorenç Balada Gaggioli <llorebaga@users.noreply.github.com>
2026-08-03 15:43:42 +0200

    Deploy Roman Campaign Atlas

---
+++ _next/static/fJj4jBmhmIZtEI7JYpsKm/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/RomanBattlesAtlas//_next/:path+",
+        "destination": "/RomanBattlesAtlas/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
